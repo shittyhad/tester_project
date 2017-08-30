57b62c3354db594bc0e1ab11b0c5e7e0a3b3e799
Bayu Wijaksana <wijaksanab@gmail.com>
2017-08-30 13:45:59 +0800

    Version Tester

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<title>Website Tester</title>
+</head>
+<body>
+	<!--JS COMMAND WEBSITE TESTER-->
+		<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.js"></script>
+
+
+<ul id="dropdown1" class="dropdown-content">
+  <li><a href="#!">Tester</a></li>
+  <li><a href="#!">Tester</a></li>
+  <li><a href="#!">Tester</a></li>
+</ul>
+<nav>
+  <div class="nav-wrapper green">
+    <a href="#!" class="brand-logo">WOW</a>
+    <ul class="right hide-on-med-and-down">
+      <li><a href="#">Test</a></li>
+      <li><a href="#">Test</a></li>
+      <li><a class="dropdown-button" href="#!" data-activates="dropdown1">WOW<i class="material-icons right">arrow_drop_down</i></a></li>
+    </ul>
+  </div>
+</nav>
+
+ <div class="slider">
+    <ul class="slides">
+      <li>
+        <img src="img/img1.jpg"> <!-- random image -->
+        <div class="caption center-align">
+          <h3>Angga Ganteng Banget!!</h3>
+          <h5 class="light grey-text text-lighten-3">Lorem Ipsum!!!</h5>
+        </div>
+      </li>
+      <li>
+        <img src="img/img2.jpg"> <!-- random image -->
+        <div class="caption left-align">
+          <h3>Angga Ganteng Banget!!</h3>
+          <h5 class="light grey-text text-lighten-3">Lorem Ipsum!!!</h5>
+        </div>
+      </li>
+      <li>
+        <img src="img/img3.jpg"> <!-- random image -->
+        <div class="caption right-align">
+          <h3>Angga Ganteng Banget!!</h3>
+          <h5 class="light grey-text text-lighten-3">Lorem Ipsum!!!</h5>
+        </div>
+      </li>
+      <li>
+        <img src="img/img4.jpg"> <!-- random image -->
+        <div class="caption center-align">
+          <h3>Angga Ganteng Banget!!</h3>
+          <h5 class="light grey-text text-lighten-3">Lorem Ipsum!!!</h5>
+        </div>
+      </li>
+    </ul>
+  </div>
+        
+</body>
+</html>
